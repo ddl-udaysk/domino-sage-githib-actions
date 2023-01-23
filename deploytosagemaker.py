da20e8d0541a32d7f17b30444aaee05a498676a1
ddl-udaysk <udaysk@dominodatalab.com>
2023-01-23 12:13:01 -0500

    test1

--- deploytosagemaker.py
+++ deploytosagemaker.py
@@ -22,11 +22,17 @@ try:
     print ("create_model API response", create_model_api_response)
 except Exception:
     print("model already present")
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
     client.delete_model(ModelName=modelName)
-    client.delete_endpoint(EndpointName=modelName+  "endpoint" )
-    client.delete_endpoint_config(EndpointConfigName=modelName+  "endpoint-configuration")
+    #client.delete_endpoint(EndpointName=modelName+  "endpoint" )
+    #client.delete_endpoint_config(EndpointConfigName=modelName+  "endpoint-configuration")
 
     time.sleep(240)
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
     create_model_api_response = client.create_model(
                                     ModelName=modelName,
                                     PrimaryContainer={
@@ -59,8 +65,10 @@ try:
     print ("create_endpoint_config API response", create_endpoint_config_api_response)
     
 except Exception:
-    client.delete_endpoint(EndpointName=modelName+  "endpoint" )
+    #client.delete_endpoint(EndpointName=modelName+  "endpoint" )
     client.delete_endpoint_config(EndpointConfigName=modelName+  "endpoint-configuration")
+    
+    time.sleep(120)
     create_endpoint_config_api_response = client.create_endpoint_config(
                                             EndpointConfigName=endpointconfiguration,
                                             ProductionVariants=[
@@ -87,6 +95,7 @@ try:
     print ("create_endpoint API response", create_endpoint_api_response) 
 except Exception:
     client.delete_endpoint(EndpointName=modelName+  "endpoint" )  
+    time.sleep(60)
     create_endpoint_api_response = client.create_endpoint(
                                     EndpointName=endpoint,
                                     EndpointConfigName=endpointconfiguration,
